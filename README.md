7dd38373f31a4911a46f6bab073301b6be7010bf
Giuseppe Rota <403432+grota@users.noreply.github.com>
2022-03-11 22:44:15 +0100

    implementation

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # fzf-helper
 
-fzf helper is a small nodejs project that I use on my personal machine to show a preview in fzf that shows the bindings of the current fzf invocation
+fzf helper is a small nodejs project that I use on my personal machine to show a preview in fzf listing the bindings of the current fzf invocation
 
 Use the following snippet in you shell init files.
 
 ```
-export FZF_DEFAULT_OPTS=--bind=\''alt-h:preview(node /path/to/fzf-helper/dist/tsc/main.js)'\'
+export FZF_DEFAULT_OPTS="--bind='alt-h:preview(/path/to/fzf-helper/dist/tsc/main.js)'"
 ```
+
+It shows up like this
+
+<img src="media/preview.png"/>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,38 @@
+#!/usr/bin/env node
 import { split } from 'shellwords'
 import getopts from 'getopts'
+import Table from 'cli-table3'
+import colors from 'colors'
 
-const toparse = process.env.FZF_DEFAULT_OPTS;
-const sp = split(toparse);
-
+const sp = split(process.env.FZF_DEFAULT_OPTS);
 const options = getopts(sp);
-if (options.bind) {
-  console.log(options.bind);
-} else {
+
+if (!options.bind) {
   console.log('no fzf bind options')
+  process.exit(0)
 }
+const columns : number = <number>(<unknown>process.env.FZF_PREVIEW_COLUMNS);
+const table = new Table({
+  head: [colors.green('Command'), colors.green('Key')],
+  wordWrap:true,
+  colWidths: [columns -24, 18],
+});
+const data: { [key: string]: string[] } = {};
+
+options.bind.forEach((bindOpts: string) => {
+  // Each --bind can have a comma separated list of key:action expressions.
+  bindOpts.split(',').forEach((bindOpt: string) => {
+    // : can appear in the rhs.
+    const [key, ...rest] = bindOpt.split(':');
+    const action = rest.join(':');
+    if (!data[action]) {
+      data[action] = [];
+    }
+    data[action].push(key);
+  });
+});
+
+for (const action in data) {
+  table.push([action, data[action].join(',')]);
+}
+console.log(table.toString());
